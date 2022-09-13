5a8b514782cb3f2706bcbc1606031fea4b9c3def
Enzo BELLET <enzo.bellet@Digital-Grenoble.local>
2022-09-13 16:11:06 +0200

    depot

--- example-app/public/index.php
+++ example-app/public/index.php
@@ -44,12 +44,14 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+//$app = require_once __DIR__.'/../bootstrap/app.php';
+//
+//$kernel = $app->make(Kernel::class);
+//
+//$response = $kernel->handle(
+//    $request = Request::capture()
+//)->send();
+//
+//$kernel->terminate($request, $response);
 
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+echo "Hello World From Laravel v9.x (PHP v8.x.x)";
