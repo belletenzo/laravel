9969b07e0d29f334709bfdb67f42adca77e30b02
Enzo BELLET <enzo.bellet@Digital-Grenoble.local>
2022-09-14 16:56:53 +0200

    Mise a jour page d'accueil bootstrap

--- example-app/resources/views/homepage.blade.php
+++ example-app/resources/views/homepage.blade.php
@@ -1,7 +1,12 @@
 @include('header')
 
 <html>
-<h1 class="text-center">Homepage</h1>
+<div class="bg-image bg-danger d-flex w-100 h-100 flex-column justify-content-center align-items-center">
+    <h1 class="display-1 text-white text-center fw-bold">Home</h1>
+    <p class="lead text-white text-center">Le E-commerce d'Enzo</p>
+    <p class="lead mb-3">
+        <a href="" class=" btn btn-dk btn-secondary">Voir les articles</a>
+    </p>
+</div>
 </html>
 
-@include('footer')
--- example-app/resources/views/product-list.blade.php
+++ example-app/resources/views/product-list.blade.php
@@ -8,11 +8,14 @@
 
                 <div class="card-body ">
                         <div class="product">
+                            <form action="cart" method="POST">
                             <h3>Album Enzo</h3>
                             <p>Price : 40 €</p>
                             <p>Promo : non </p>
+                                <input type="number" name="quantity" value="0" min="0" max="99">
                             <p>Stock : 99</p>
-                            <button>Voir l'article</button>
+                                <input type="submit" value="Ajouter au panier">
+                            </form>
                         </div>
                 </div>
                 <img src="https://media.istockphoto.com/photos/blank-vinyl-album-cover-sleeve-mockup-isolated-clipping-path-picture-id536669874?k=20&m=536669874&s=612x612&w=0&h=9pvT9F_6J6kWRjBNvQCJOGeiZkeCTrm7JRgWHqcCLSQ=" alt="" class="w-100 img-fluid">
@@ -20,6 +23,7 @@
         </div>
     </div>
 </div>
+@include('footer')
 </html>
 
-@include('footer')
+
