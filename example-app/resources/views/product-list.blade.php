24a5aaf006333d2eb6465caa96edd1b2be1675c9
Enzo BELLET <enzo.bellet@Digital-Grenoble.local>
2022-09-16 09:38:19 +0200

    Etape 5

--- example-app/resources/views/product-list.blade.php
+++ example-app/resources/views/product-list.blade.php
@@ -6,26 +6,24 @@ dump($products);
 @endphp
 <div class="container-fluid w-75 mt-4">
     <div class="row">
+        @foreach($products as $product)
         <div class="col-lg-3 col-md-6 col-sm-6">
-            @foreach($products as $product)
             <div class="card mb-4 shadow-sm border border-3 border-secondary">
                 <div class="card-body ">
                     <div class="product">
                         <form action="cart" method="POST">
                             <h3>{{$product->name}}</h3>
-                            <p>{{$product->price}}</p>
-                            <p>{{$product->discount}}</p>
-                            <input type="number" name="quantity" value="0" min="0" max="99">
+                            <p>Prix : {{$product->price}} €</p>
+                            <p>Promo : {{$product->discount}}%</p>
+                            <input type="number" name="quantity" value="0" min="0" max="{{$product->quantity}}">
                             <a href="product/{id}/" class=" btn btn-dk btn-secondary">Voir l'article</a>
                         </form>
                     </div>
                 </div>
-                <img
-                    src="{{asset($product->image)}}"
-                    alt="" class="w-100 img-fluid">
+                <img src="{{$product->image}}" alt="" class="w-100 img-fluid">
             </div>
-            @endforeach
         </div>
+        @endforeach
     </div>
 </div>
 @endsection
